ead09da6416e3c1aa5e3cdc1f3a1a5017a97a37c
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-02-10 12:30:57 +0200

    CalculatorSwitch

---
+++ CalculatorSwitch/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace CalculatorSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, User!");
+
+            Console.WriteLine("Insert calculation type by number: ");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            int calculationType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert first number and press enter");
+            float number = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert second number and press enter");
+            float number2 = float.Parse(Console.ReadLine());
+
+            float result = 0;
+
+            switch(calculationType)
+            {
+                case 1:
+                    result = number + number2;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine($"{number} + {number2} = {result}");
+                    break;
+                case 2:
+                    result = number - number2;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine($"{number} - {number2} = {result}");
+                    break;       
+                case 3:
+                    result = number * number2;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine($"{number} * {number2} = {result}");
+                    break;                
+                case 4:
+                    result = number / number2;
+                    Console.WriteLine("Answer: ");
+                    Console.WriteLine($"{number} / {number2} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
